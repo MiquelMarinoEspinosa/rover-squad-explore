e520ffbbf82fd4aea8fbea3958bcc0c7d699aaf8
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-09-28 15:43:09 +0200

    refactor coordinate folder name for point folder name

--- Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Area/RectangularArea.php
+++ Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Area/RectangularArea.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Core\RoverControlPanel\Domain\Rover\Position\Coordinate\Area;
+namespace Core\RoverControlPanel\Domain\Rover\Position\Point\Area;
 
 final readonly class RectangularArea implements Area
 {
--- Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Position/Point/Area/RectangularAreaTest.php
+++ Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Position/Point/Area/RectangularAreaTest.php
@@ -2,17 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Core\Tests\Unit\Domain\Rover\Position\Coordinate\Area;
+namespace Core\Tests\Unit\Domain\Rover\Position\Point\Area;
 
-use Core\RoverControlPanel\Domain\Rover\Position\Coordinate\Area\Area;
-use Core\RoverControlPanel\Domain\Rover\Position\Coordinate\Area\RectangularArea;
+use Core\RoverControlPanel\Domain\Rover\Position\Point\Area\Area;
+use Core\RoverControlPanel\Domain\Rover\Position\Point\Area\RectangularArea;
 use PHPUnit\Framework\TestCase;
 
 final class RectangularAreaTest extends TestCase
 {
     public function testShouldCreateTheArea(): void
     {
-        $map = new RectangularArea();
+        $map = new RectangularArea(
+
+        );
 
         self::assertInstanceOf(RectangularArea::class, $map);
         self::assertInstanceOf(Area::class, $map);
