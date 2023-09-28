7e58d44d8659a4b3827ed9479e1e401133ea9a93
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-09-28 14:36:35 +0200

    create first unit test

---
+++ Backend/src/Rover/Tests/Unit/Domain/Position/Map/MapTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Rover\Tests\Unit\Domain\Position\Map;
+
+use PHPUnit\Framework\TestCase;
+
+final class MapTest extends TestCase
+{
+    public function testAssertFalse(): void
+    {
+        self::assertFalse(false);
+    }
+}
