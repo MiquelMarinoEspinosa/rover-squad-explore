5ebd06c031117c9a73f4e27aeba11562e46ab228
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-09-29 09:41:02 +0200

    create Coordinate interface

--- Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Coordinate/Coordinate.php
+++ Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Coordinate/Coordinate.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Core\RoverControlPanel\Domain\Rover\Position\Point\Coordinate;
 
-final readonly class AbscissaCoordinate implements Coordinate
+interface Coordinate
 {
+    
 }
--- Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Position/Point/Coordinate/AbscissaCoordinateTest.php
+++ Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Position/Point/Coordinate/AbscissaCoordinateTest.php
@@ -3,14 +3,16 @@
 namespace Core\RoverControlPanel\Tests\Unit\Domain\Rover\Position\Point\Coordinate;
 
 use Core\RoverControlPanel\Domain\Rover\Position\Point\Coordinate\AbscissaCoordinate;
+use Core\RoverControlPanel\Domain\Rover\Position\Point\Coordinate\Coordinate;
 use PHPUnit\Framework\TestCase;
 
 final class AbscissaCoordinateTest extends TestCase
 {
     public function testShouldCreateAbscissaCoordinate(): void
     {
-        $abscissaCoordinate = new AbscissaCoordinate(0);
+        $abscissaCoordinate = new AbscissaCoordinate();
 
         self::assertInstanceOf(AbscissaCoordinate::class, $abscissaCoordinate);
-    }    
+        self::assertInstanceOf(Coordinate::class, $abscissaCoordinate);
+    }
 }
