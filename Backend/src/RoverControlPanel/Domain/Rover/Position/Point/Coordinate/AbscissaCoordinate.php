0e73957a2051368051c47f146fbd953b790f6f29
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-09-29 10:41:45 +0200

    pass error message abscissa not allowed exception

--- Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Coordinate/AbscissaCoordinate.php
+++ Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Coordinate/AbscissaCoordinate.php
@@ -6,7 +6,8 @@ namespace Core\RoverControlPanel\Domain\Rover\Position\Point\Coordinate;
 
 final readonly class AbscissaCoordinate implements Coordinate
 {
-    private const MOVEMENT_VALUE = 1; 
+    private const MOVEMENT_VALUE = 1;
+    private const ERROR_MESSAGE  = 'AbscissaCoordinate operation not allowed: ';
 
     public function __construct(
         private int $value
@@ -15,12 +16,16 @@ final readonly class AbscissaCoordinate implements Coordinate
 
     public function moveUp(): self
     {
-        throw new NotAllowedMovement();
+        throw new NotAllowedMovement(
+            self::ERROR_MESSAGE . 'moveUp'
+        );
     }
 
     public function moveDown(): self
     {
-        throw new NotAllowedMovement();
+        throw new NotAllowedMovement(
+            self::ERROR_MESSAGE . 'moveDow'
+        );
     }
 
     public function moveRight(): self
