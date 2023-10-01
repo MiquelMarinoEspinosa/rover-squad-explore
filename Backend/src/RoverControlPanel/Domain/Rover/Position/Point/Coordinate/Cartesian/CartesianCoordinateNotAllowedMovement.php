55e6aa66484acb8b8208e84916cd2be595c7bb15
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-01 10:01:14 +0200

    refactor create method CartesianCoordinateNotAllowedMovement exception

--- Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Coordinate/Cartesian/CartesianCoordinateNotAllowedMovement.php
+++ Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Coordinate/Cartesian/CartesianCoordinateNotAllowedMovement.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace Core\RoverControlPanel\Domain\Rover\Position\Point\Coordinate\Cartesian;
 
+use Throwable;
+
 final class CartesianCoordinateNotAllowedMovement extends \Exception
 {
+    private const ERROR_MESSAGE = 'CartesianCoordinate movement not allowed: ';
+
+    public function _construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+
+    }
+
     public static function create(
         string $movement
     ): self {
-        return new self();
+        return new self(
+            self::ERROR_MESSAGE . $movement
+        );
     }
 }
