a03cfbfe4be3a719300ab85ff06a1425cf58b6fc
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-01 10:23:39 +0200

    implements CartesianCoordinatePointData interface

--- Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Cartesian/CartesianPointData.php
+++ Backend/src/RoverControlPanel/Domain/Rover/Position/Point/Cartesian/CartesianPointData.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Core\RoverControlPanel\Domain\Rover\Position\Point\Cartesian;
 
-final readonly class CartesianCoordinatePointData implements CartesianPointData
+interface CartesianPointData
 {
-    
 }
--- Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Position/Point/Cartesian/CartesianCoordinatePointDataTest.php
+++ Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Position/Point/Cartesian/CartesianCoordinatePointDataTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Core\RoverControlPanel\Tests\Unit\Domain\Rover\Position\Point\Cartesian;
 
 use PHPUnit\Framework\TestCase;
+use Core\RoverControlPanel\Domain\Rover\Position\Point\Cartesian\CartesianPointData;
 use Core\RoverControlPanel\Domain\Rover\Position\Point\Cartesian\CartesianCoordinatePointData;
 
 final class CartesianCoordinatePointDataTest extends TestCase
@@ -15,6 +16,11 @@ final class CartesianCoordinatePointDataTest extends TestCase
 
         self::assertInstanceOf(
             CartesianCoordinatePointData::class,
+            $cartesianCoordinatePointData
+        );
+
+        self::assertInstanceOf(
+            CartesianPointData::class,
             $cartesianCoordinatePointData
         );
     }
