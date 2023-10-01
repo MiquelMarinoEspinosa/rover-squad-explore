341d43d8619d815d8fa9a74005df2474870c7928
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-01 19:30:07 +0200

    add cartesian point as RectangularCartesianInvalidArea create method

--- Backend/src/RoverControlPanel/Domain/Rover/Area/Cartesian/Rectangular/RectangularCartesianInvalidArea.php
+++ Backend/src/RoverControlPanel/Domain/Rover/Area/Cartesian/Rectangular/RectangularCartesianInvalidArea.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Core\RoverControlPanel\Domain\Rover\Area\Cartesian\Rectangular;
 
 use Core\RoverControlPanel\Domain\Rover\Area\InvalidArea;
+use Core\RoverControlPanel\Domain\Rover\Point\Cartesian\CartesianPoint;
 use Exception;
 
 final class RectangularCartesianInvalidArea extends Exception implements InvalidArea
 {
-    public static function create(): self
-    {
+    public static function create(
+        CartesianPoint $invalidPoint
+    ): self {
         return new self();
     }
 }
--- Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Area/Cartesian/Rectangular/InvalidRectangularCartesianAreaTest.php
+++ Backend/src/RoverControlPanel/Tests/Unit/Domain/Rover/Area/Cartesian/Rectangular/InvalidRectangularCartesianAreaTest.php
@@ -7,13 +7,22 @@ namespace Core\RoverControlPanel\Tests\Unit\Domain\Rover\Area\Cartesian\Rectangu
 use Throwable;
 use PHPUnit\Framework\TestCase;
 use Core\RoverControlPanel\Domain\Rover\Area\InvalidArea;
+use Core\RoverControlPanel\Domain\Rover\Point\Cartesian\CartesianCoordinatePoint;
 use Core\RoverControlPanel\Domain\Rover\Area\Cartesian\Rectangular\RectangularCartesianInvalidArea;
 
 final class InvalidRectangularCartesianAreaTest extends TestCase
 {
+    private const INVALID_UPPER_RIGHT_ABSCISSA = -1;
+    private const INVALID_UPPER_RIGHT_ORDINADA = self::INVALID_UPPER_RIGHT_ABSCISSA;
+
     public function testShouldCreateInvalidRectangularCartesianArea(): void
     {
-        $rectangularCartesianInvalidArea = RectangularCartesianInvalidArea::create();
+        $rectangularCartesianInvalidArea = RectangularCartesianInvalidArea::create(
+            CartesianCoordinatePoint::create(
+                self::INVALID_UPPER_RIGHT_ABSCISSA,
+                self::INVALID_UPPER_RIGHT_ORDINADA
+            )
+        );
 
         self::assertInstanceOf(
             RectangularCartesianInvalidArea::class,
