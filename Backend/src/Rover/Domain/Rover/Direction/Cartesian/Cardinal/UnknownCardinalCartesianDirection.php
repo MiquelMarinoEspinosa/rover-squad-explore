234094846f433bda8e0681041c4aaadcf162fa05
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-02 14:37:00 +0200

    implements unknown cardinal construct method

--- Backend/src/Rover/Domain/Rover/Direction/Cartesian/Cardinal/UnknownCardinalCartesianDirection.php
+++ Backend/src/Rover/Domain/Rover/Direction/Cartesian/Cardinal/UnknownCardinalCartesianDirection.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 namespace Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal;
 
 use Exception;
+use Throwable;
 use Core\Rover\Domain\Rover\Direction\UnknownDirection;
 
 final class UnknownCardinalCartesianDirection extends Exception implements UnknownDirection
 {
+    private const ERROR_MESSAGE = 'Unknown direction: %s';
+
+    private function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
     public static function create(string $value): self
     {
-        return new self();
+        return new self(
+            sprintf(self::ERROR_MESSAGE, $value)
+        );
     }
 }
