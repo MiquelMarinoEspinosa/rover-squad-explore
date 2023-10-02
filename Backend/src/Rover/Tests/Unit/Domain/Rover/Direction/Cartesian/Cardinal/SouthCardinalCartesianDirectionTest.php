fdaa6ab87cd58310e13714308dd915217624b37b
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-02 09:50:24 +0200

    implements South cardinal

---
+++ Backend/src/Rover/Tests/Unit/Domain/Rover/Direction/Cartesian/Cardinal/SouthCardinalCartesianDirectionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Rover\Tests\Unit\Domain\Rover\Direction\Cartesian\Cardinal;
+
+use PHPUnit\Framework\TestCase;
+use Core\Rover\Domain\Rover\Direction\Direction;
+use Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal\CartesianCardinalDirection;
+use Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal\SouthCardinalCartesianDirection;
+
+final class SouthCardinalCartesianDirectionTest extends TestCase
+{
+    public function testShouldCreateSouthCartesianCardinalDirection(): void
+    {
+        $southCartesianCardinalDirection = new SouthCardinalCartesianDirection();
+
+        self::assertInstanceOf(
+            SouthCardinalCartesianDirection::class,
+            $southCartesianCardinalDirection
+        );
+
+        self::assertInstanceOf(
+            CartesianCardinalDirection::class,
+            $southCartesianCardinalDirection
+        );
+
+        self::assertInstanceOf(
+            Direction::class,
+            $southCartesianCardinalDirection
+        );
+    }
+}
