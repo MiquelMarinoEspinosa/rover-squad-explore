1e24e57b43eedb4ed2ad8c444686a5f1471ab441
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-02 09:52:34 +0200

    creates west cardinal

---
+++ Backend/src/Rover/Tests/Unit/Domain/Rover/Direction/Cartesian/Cardinal/WestCardinalCartesianDirectionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Rover\Tests\Unit\Domain\Rover\Direction\Cartesian\Cardinal;
+
+use PHPUnit\Framework\TestCase;
+use Core\Rover\Domain\Rover\Direction\Direction;
+use Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal\CartesianCardinalDirection;
+use Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal\WestCardinalCartesianDirection;
+
+final class WestCardinalCartesianDirectionTest extends TestCase
+{
+    public function testShouldCreateWestCartesianCardinalDirection(): void
+    {
+        $southCartesianCardinalDirection = new WestCardinalCartesianDirection();
+
+        self::assertInstanceOf(
+            WestCardinalCartesianDirection::class,
+            $southCartesianCardinalDirection
+        );
+
+        self::assertInstanceOf(
+            CartesianCardinalDirection::class,
+            $southCartesianCardinalDirection
+        );
+
+        self::assertInstanceOf(
+            Direction::class,
+            $southCartesianCardinalDirection
+        );
+    }
+}
