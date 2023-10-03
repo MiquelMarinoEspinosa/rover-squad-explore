e2492cecdc17cec32a221106cc56357f8caa1e14
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-03 13:51:36 +0200

    implements CartesianCardinalCoordinateRoverBuilderDataMapper

--- Backend/src/Rover/Application/Explore/Mapper/Rover/RoverBuilderDataMapper.php
+++ Backend/src/Rover/Application/Explore/Mapper/Rover/RoverBuilderDataMapper.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Core\Rover\Application\Explore\Mapper\Rover;
 
+use Core\Rover\Domain\Rover\RoverBuilderData;
+
 interface RoverBuilderDataMapper
 {
+    public function map(): RoverBuilderData;
 }
--- Backend/src/Rover/Tests/Unit/Application/Explore/Mapper/Rover/Cartesian/Cardinal/Coordinate/CartesianCardinalCoordinateRoverBuilderDataMapperTest.php
+++ Backend/src/Rover/Tests/Unit/Application/Explore/Mapper/Rover/Cartesian/Cardinal/Coordinate/CartesianCardinalCoordinateRoverBuilderDataMapperTest.php
@@ -7,12 +7,24 @@ namespace Core\Rover\Tests\Unit\Application\Explore\Mapper\Rover\Cartesian\Cardi
 use PHPUnit\Framework\TestCase;
 use Core\Rover\Application\Explore\Mapper\Rover\RoverBuilderDataMapper;
 use Core\Rover\Application\Explore\Mapper\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverBuilderDataMapper;
+use Core\Rover\Application\Explore\Request\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverDiscoverRequest;
+use Core\Rover\Domain\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverBuilderData;
 
 final class CartesianCardinalCoordinateRoverBuilderDataMapperTest extends TestCase
 {
+    private const AREA_UPPER_RIGHT_ABSCISSA = 5;
+    private const AREA_UPPER_RIGHT_ORDINATE = 6;
+    private const POSITION_CARDINAL         = 'N';
+    private const POSITION_ABSCISSA         = 0;
+    private const POSITION_ORDINATE         = 1;
+
     public function testCreateTheCartesianCardinalCoordinateRoverBuilderDataMapper(): void
     {
-        $mapper = new CartesianCardinalCoordinateRoverBuilderDataMapper;
+        $request = $this->givenRequest();
+
+        $mapper = new CartesianCardinalCoordinateRoverBuilderDataMapper(
+            $request
+        );
 
         self::assertInstanceOf(
             CartesianCardinalCoordinateRoverBuilderDataMapper::class,
@@ -22,6 +34,44 @@ final class CartesianCardinalCoordinateRoverBuilderDataMapperTest extends TestCa
         self::assertInstanceOf(
             RoverBuilderDataMapper::class,
             $mapper
+        );
+    }
+
+    public function testShouldMapTheRoverBuilderData(): void
+    {
+        $request = $this->givenRequest();
+
+        $expectedRoverBuilderData = $this->givenExpectedRoverBuilderData();
+
+        $roverBuilderDataMapper = new CartesianCardinalCoordinateRoverBuilderDataMapper(
+            $request
+        );
+
+        self::assertEquals(
+            $expectedRoverBuilderData,
+            $roverBuilderDataMapper->map()
+        );
+    }
+
+    private function givenRequest(): CartesianCardinalCoordinateRoverDiscoverRequest
+    {
+        return new CartesianCardinalCoordinateRoverDiscoverRequest(
+            self::AREA_UPPER_RIGHT_ABSCISSA,
+            self::AREA_UPPER_RIGHT_ORDINATE,
+            self::POSITION_CARDINAL,
+            self::POSITION_ABSCISSA,
+            self::POSITION_ORDINATE
+        );
+    }
+
+    private function givenExpectedRoverBuilderData(): CartesianCardinalCoordinateRoverBuilderData
+    {
+        return new CartesianCardinalCoordinateRoverBuilderData(
+            self::AREA_UPPER_RIGHT_ABSCISSA,
+            self::AREA_UPPER_RIGHT_ORDINATE,
+            self::POSITION_CARDINAL,
+            self::POSITION_ABSCISSA,
+            self::POSITION_ORDINATE
         );
     }
 }
