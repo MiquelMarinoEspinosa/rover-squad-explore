045048207926780f6768f9dcc7c3fa9797c8318d
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-03 09:50:35 +0200

    implements RoverSquadOutOfRange construct

--- Backend/src/Rover/Domain/Rover/RoverSquadOutOfRange.php
+++ Backend/src/Rover/Domain/Rover/RoverSquadOutOfRange.php
@@ -6,11 +6,21 @@ namespace Core\Rover\Domain\Rover;
 
 use Core\Rover\Domain\CollectionOutOfRange;
 use Exception;
+use Throwable;
 
 final class RoverSquadOutOfRange extends Exception implements CollectionOutOfRange
 {
+    private const MESSAGE = 'Rover squad out of range';
+
+    private function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
     public static function create(): self
     {
-        return new self;
+        return new self(
+            self::MESSAGE
+        );
     }
 }
