f8f095046c3f22645406dce008a615386a72f576
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-03 13:36:09 +0200

    implements CartesianCardinalCoordinateRoverDiscoverRequest

--- Backend/src/Rover/Tests/Unit/Application/Explore/Request/Rover/Cartesian/Cardinal/Coordinate/CartesianCardinalCoordinateRoverDiscoverRequestTest.php
+++ Backend/src/Rover/Tests/Unit/Application/Explore/Request/Rover/Cartesian/Cardinal/Coordinate/CartesianCardinalCoordinateRoverDiscoverRequestTest.php
@@ -10,9 +10,34 @@ use PHPUnit\Framework\TestCase;
 
 final class CartesianCardinalCoordinateRoverDiscoverRequestTest extends TestCase
 {
+    private const AREA_UPPER_RIGHT_ABSCISSA = 5;
+    private const AREA_UPPER_RIGHT_ORDINATE = 6;
+    private const POSITION_CARDINAL         = 'N';
+    private const POSITION_ABSCISSA         = 0;
+    private const POSITION_ORDINATE         = 1;
+
+    private CartesianCardinalCoordinateRoverDiscoverRequest $roverDiscoverRequest;
+
+    protected function setUp(): void 
+    {
+        $this->roverDiscoverRequest = new CartesianCardinalCoordinateRoverDiscoverRequest(
+            self::AREA_UPPER_RIGHT_ABSCISSA,
+            self::AREA_UPPER_RIGHT_ORDINATE,
+            self::POSITION_CARDINAL,
+            self::POSITION_ABSCISSA,
+            self::POSITION_ORDINATE
+        );
+    }
+
     public function testShouldCreateTheRequest(): void
     {
-        $request = new CartesianCardinalCoordinateRoverDiscoverRequest;
+        $request = new CartesianCardinalCoordinateRoverDiscoverRequest(
+            self::AREA_UPPER_RIGHT_ABSCISSA,
+            self::AREA_UPPER_RIGHT_ORDINATE,
+            self::POSITION_CARDINAL,
+            self::POSITION_ABSCISSA,
+            self::POSITION_ORDINATE
+        );
 
         self::assertInstanceOf(
             CartesianCardinalCoordinateRoverDiscoverRequest::class,
@@ -22,6 +47,46 @@ final class CartesianCardinalCoordinateRoverDiscoverRequestTest extends TestCase
         self::assertInstanceOf(
             RoverDiscoverRequest::class,
             $request
+        );
+    }
+
+    public function testShouldReturnTheAreaUpperRightAbscissa(): void
+    {
+        self::assertSame(
+            self::AREA_UPPER_RIGHT_ABSCISSA,
+            $this->roverDiscoverRequest->areaUpperRightAbscissa()
+        );
+    }
+
+    public function testShouldReturnTheAreaUpperRightOrdinate(): void
+    {
+        self::assertSame(
+            self::AREA_UPPER_RIGHT_ORDINATE,
+            $this->roverDiscoverRequest->areaUpperRightOrdinate()
+        );
+    }
+
+    public function testShouldReturnThePositionCardinal(): void
+    {
+        self::assertSame(
+            self::POSITION_CARDINAL,
+            $this->roverDiscoverRequest->positionCardinal()
+        );
+    }
+
+    public function testShouldReturnThePositionAbscissa(): void
+    {
+        self::assertSame(
+            self::POSITION_ABSCISSA,
+            $this->roverDiscoverRequest->positionAbscissa()
+        );
+    }
+
+    public function testShouldReturnThePositionOrdinate(): void
+    {
+        self::assertSame(
+            self::POSITION_ORDINATE,
+            $this->roverDiscoverRequest->positionOrdinate()
         );
     }
 }
