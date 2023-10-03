b4324fb3189137913c8a5d99fd52dbf977e716ec
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-03 11:25:04 +0200

    implements MovementCollectionOutOfRange construct method

--- Backend/src/Rover/Domain/Movement/MovementCollectionOutOfRange.php
+++ Backend/src/Rover/Domain/Movement/MovementCollectionOutOfRange.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace Core\Rover\Domain\Movement;
 
-use Core\Rover\Domain\Collection\CollectionOutOfRange;
 use Exception;
+use Throwable;
+use Core\Rover\Domain\Collection\CollectionOutOfRange;
 
 final class MovementCollectionOutOfRange extends Exception implements CollectionOutOfRange
 {
+    private const MESSAGE = 'Movement collection out of range';
+
+    private function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
     public static function create(): self
     {
-        return new self;
+        return new self(
+            self::MESSAGE
+        );
     }
 }
