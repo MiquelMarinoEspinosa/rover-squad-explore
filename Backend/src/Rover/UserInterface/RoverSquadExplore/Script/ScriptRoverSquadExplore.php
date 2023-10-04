6584a7b922637677cf7986658835b87af7286ca2
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-04 11:56:36 +0200

    format parameters

--- Backend/src/Rover/UserInterface/RoverSquadExplore/Script/ScriptRoverSquadExplore.php
+++ Backend/src/Rover/UserInterface/RoverSquadExplore/Script/ScriptRoverSquadExplore.php
@@ -1,35 +1,53 @@
 <?php
 
-function explore($parameters) {
+use Core\Rover\Application\RoverSquadExplore\Request\Movement\Cartesian\CartesianMovementExploreRequest;
+use Core\Rover\Application\RoverSquadExplore\Request\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverExploreRequest;
+use Core\Rover\Application\RoverSquadExplore\Request\RoverSquadExploreRequest;
+use Core\Rover\Application\RoverSquadExplore\RoverSquadExploreUseCaseRegistry;
+
+function explore($parameters)
+{
     $abscissaUpperRightArea = $parameters[0];
     $ordinateUpperRightArea = $parameters[1];
 
-    var_dump($abscissaUpperRightArea);
-    var_dump($ordinateUpperRightArea);
-
-    var_dump($parameters);
-    //var_dump(count($parameters));
-
-    unset($parameters[0]);
-    unset($parameters[1]); 
     $roverMovementsParametersLenght = 0;
-    
-    foreach ($parameters as $parameter)
-    {
+
+    foreach ($parameters as $parameter) {
         $roverMovementsParametersLenght++;
     }
 
-    $paramIndex = 2;
-    
-    while ($paramIndex < $roverMovementsParametersLenght)
-    {
-        var_dump($parameters[$paramIndex]);
-        var_dump($parameters[$paramIndex + 1]);
-        var_dump($parameters[$paramIndex + 2]);
-        var_dump($parameters[$paramIndex + 3]);
-        $paramIndex = $paramIndex + 4;
+    $roverMovementParamIndex = 2;
+    $roverExploreRequests               = [];
+    $movementExploreCollectionRequests  = [];
+
+    while ($roverMovementParamIndex < $roverMovementsParametersLenght) {
+        $movementExploreRequests = [];
+        $roverExploreRequests[] = new CartesianCardinalCoordinateRoverExploreRequest(
+            $abscissaUpperRightArea,
+            $ordinateUpperRightArea,
+            $parameters[$roverMovementParamIndex],
+            $parameters[$roverMovementParamIndex + 1],
+            $parameters[$roverMovementParamIndex + 2]
+        );
+
+        foreach (str_split($parameters[$roverMovementParamIndex + 3]) as $movementValue) {
+            $movementExploreRequests[] = new CartesianMovementExploreRequest(
+                $movementValue
+            );
+        }
+        $movementExploreCollectionRequests[] = $movementExploreRequests;
+        $roverMovementParamIndex += 4;
     }
 
+    $roverSquadExploreUseCase = RoverSquadExploreUseCaseRegistry::getInstance()
+        ->get('cartesian.coordinate.rover_squad_explore_use_case');
+
+    $request = new RoverSquadExploreRequest(
+        $roverExploreRequests,
+        $movementExploreCollectionRequests
+    );
+
+    $response = $roverSquadExploreUseCase->execute($request);
 
     return new stdClass;
 }
