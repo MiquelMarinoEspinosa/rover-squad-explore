e4d2fa8aac4c47cc2de93b6452230bd5c5d744cd
miguel_marino <miguel.alejandro.marino.espinosa@gmail.com>
2023-10-04 09:57:38 +0200

    implements registry get method

--- Backend/src/Rover/Application/RoverSquadExplore/RoverSquadExploreUseCaseRegistry.php
+++ Backend/src/Rover/Application/RoverSquadExplore/RoverSquadExploreUseCaseRegistry.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace Core\Rover\Application\RoverSquadExplore;
 
+use Core\Rover\Application\RoverSquadExplore\Request\Mapper\Movement\Cartesian\CartesianMovementFactoryDataMapper;
+use Core\Rover\Application\RoverSquadExplore\Request\Mapper\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverBuilderDataMapper;
+use Core\Rover\Application\RoverSquadExplore\Response\Mapper\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverExploreResponseMapper;
+use Core\Rover\Domain\Movement\Cartesian\CartesianMovementFactory;
+use Core\Rover\Domain\Rover\Area\Cartesian\Rectangular\RectangularCartesianAreaBuilder;
+use Core\Rover\Domain\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverBuilder;
+use Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal\CartesianCardinalDirectionFactory;
+
 final class RoverSquadExploreUseCaseRegistry
 {
     private static ?self $instance = null;
+    private static $roverSquadExploreUseCases = [];
     
+    private function __construct()
+    {
+        self::$roverSquadExploreUseCases[
+            'cartesian.coordinate.rover_squad_explore_use_case'
+        ] = $this->buildCartesianCoordinateRoverSquadUseCase();
+    }
+
     public static function getInstance(): self
     {
         if (null !== self::$instance)
@@ -18,5 +34,28 @@ final class RoverSquadExploreUseCaseRegistry
         self::$instance = new self;
 
         return self::$instance;
+    }
+
+    public function get(
+        string $roverSquadExploreUseCaseName
+    ): RoverSquadExploreUseCase {
+        
+        return self::$roverSquadExploreUseCases[
+            $roverSquadExploreUseCaseName
+        ];
+    }
+
+    private static function buildCartesianCoordinateRoverSquadUseCase(): RoverSquadExploreUseCase
+    {
+        return new RoverSquadExploreUseCase(
+            new CartesianCardinalCoordinateRoverBuilderDataMapper,
+            new CartesianCardinalCoordinateRoverBuilder(
+                RectangularCartesianAreaBuilder::getInstance(),
+                CartesianCardinalDirectionFactory::getInstance()
+            ),
+            new CartesianMovementFactoryDataMapper,
+            CartesianMovementFactory::getInstance(),
+            new CartesianCardinalCoordinateRoverExploreResponseMapper
+        );
     }
 }
--- Backend/src/Rover/Tests/Unit/Application/RoverSquadExplore/RoverSquadExploreUseCaseRegistryTest.php
+++ Backend/src/Rover/Tests/Unit/Application/RoverSquadExplore/RoverSquadExploreUseCaseRegistryTest.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace Core\Rover\Tests\Unit\Application\RoverSquadExplore;
 
+use Core\Rover\Application\RoverSquadExplore\Request\Mapper\Movement\Cartesian\CartesianMovementFactoryDataMapper;
+use Core\Rover\Application\RoverSquadExplore\Request\Mapper\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverBuilderDataMapper;
+use Core\Rover\Application\RoverSquadExplore\Response\Mapper\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverExploreResponseMapper;
+use Core\Rover\Application\RoverSquadExplore\RoverSquadExploreUseCase;
 use Core\Rover\Application\RoverSquadExplore\RoverSquadExploreUseCaseRegistry;
+use Core\Rover\Domain\Movement\Cartesian\CartesianMovementFactory;
+use Core\Rover\Domain\Rover\Area\Cartesian\Rectangular\RectangularCartesianAreaBuilder;
+use Core\Rover\Domain\Rover\Cartesian\Cardinal\Coordinate\CartesianCardinalCoordinateRoverBuilder;
+use Core\Rover\Domain\Rover\Direction\Cartesian\Cardinal\CartesianCardinalDirectionFactory;
 use PHPUnit\Framework\TestCase;
 
 final class RoverSquadExploreUseCaseRegistryTest extends TestCase
@@ -24,6 +32,34 @@ final class RoverSquadExploreUseCaseRegistryTest extends TestCase
         self::assertSame(
             RoverSquadExploreUseCaseRegistry::getInstance(),
             RoverSquadExploreUseCaseRegistry::getInstance()
+        );
+    }
+
+    public function tesThouldReturnTheCartesianCoordinateRoverSquadExploreUseCase(): void
+    {
+        $registry = RoverSquadExploreUseCaseRegistry::getInstance();
+
+        $useCase = $registry->get('cartesian.coordinate.rover_squad_explore_use_case');
+
+        $expectedUseCase = $this->buildCartesianCoordinateRoverSquadUseCase();
+
+        self::assertEquals(
+            $expectedUseCase,
+            $useCase
+        );
+    }
+
+    private function buildCartesianCoordinateRoverSquadUseCase(): RoverSquadExploreUseCase
+    {
+        return new RoverSquadExploreUseCase(
+            new CartesianCardinalCoordinateRoverBuilderDataMapper,
+            new CartesianCardinalCoordinateRoverBuilder(
+                RectangularCartesianAreaBuilder::getInstance(),
+                CartesianCardinalDirectionFactory::getInstance()
+            ),
+            new CartesianMovementFactoryDataMapper,
+            CartesianMovementFactory::getInstance(),
+            new CartesianCardinalCoordinateRoverExploreResponseMapper
         );
     }
 }
